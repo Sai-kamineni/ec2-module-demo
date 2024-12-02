3c294baed9f589f27a82edaab3dca78e1d451e31
Sai-kamineni <saikumar.dba10@gmail.com>
2024-12-02 08:54:07 +0530

    dynamic

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 module "ec2" {
-    source = "../terraform-aws-ec2"
+    #source = "../terraform-aws-ec2"
+    source = "git::https://github.com/Sai-kamineni/terraform-aws-ec2.git"
     # these are variables expected module, not arguments of resource definition
     # You can also create variables or else you can directly use values here
     ami_id = var.ami_something
